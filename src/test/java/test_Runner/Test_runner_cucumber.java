45ff0b41abd12e08386583d2e3132d2e8b180d6a
Suryanarayana-Reddy <suryanarayanareddypulagam99@gmail.com>
2020-12-08 22:35:52 +0530

    suryaprojects

--- src/test/java/test_Runner/Test_runner_cucumber.java
+++ src/test/java/test_Runner/Test_runner_cucumber.java
@@ -3,6 +3,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class) 
 @CucumberOptions(features="src/test/java/featuredemos", glue={"step_definition"},
+plugin = {"json:target/cucumber.json"},
 //tags={"@Add_to_cart"}
 tags={"@Successful_login,@Unsuccessful_login"})
 //tags={"@Successful_Registration,@first_name_field_validation,@last_name_field_validation,@email_field_validation,@password_field_validation,@confirm_password_field_validation"})
